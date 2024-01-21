818944fe963c477aaa85769c1ccb787c5130ca54
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2024-01-22 00:21:52 +0530

    add contacts page

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Root from "./routes/root";
+import Root, { loader as rootLoader } from "./routes/root";
 import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
-    { path: "/", element: <Root/>,errorElement:<ErrorPage/> },
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        loader: rootLoader,
+        children: [
+            {
+                path: "contacts/:contactId",
+                element: <Contact />,
+            },
+        ],
+    },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
